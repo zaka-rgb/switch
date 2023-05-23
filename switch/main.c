fa31c32f0a5258fac4b3e90c4f2ff9c3c14401b9
Zakaria Ben <75684862+zaka-rgb@users.noreply.github.com>
2023-05-23 23:23:32 +0200

    Add files via upload

---
+++ switch/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int choix=0;
+ printf("------MENU------\n");
+printf("1.SMA\n");
+printf("2.SMI\n");
+printf("3.SMP\n");
+printf("4.SMC\n");
+printf("entrez votre choix\n");
+scanf("%d",&choix);
+switch(choix)
+{
+case 1:
+    printf("waaa l3addaaaab");
+    break;
+case 2:
+    printf("hhhh,l3adaaaaab*l3adaaaaab");
+    break;
+case 3:
+    printf("hhhhh,lah i7ssan 3wank ");
+    break;
+case 4:
+    printf("hhhhhh,mchiti fiha");
+    break;
+default:
+printf("error");
+break;
+}
+}
